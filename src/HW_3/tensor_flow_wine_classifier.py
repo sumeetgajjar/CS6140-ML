b8eb44a77a6c9944aeb4248d9ede75c25edcb2b8
gajjar.s <gajjar.s@husky.neu.edu>
2019-02-14 14:35:54 -0500

    MLE wine classifier done

--- src/HW_3/tensor_flow_wine_classifier.py
+++ src/HW_3/tensor_flow_wine_classifier.py
@@ -96,8 +96,7 @@ class MleWineClassifier(WineClassifier):
         return hidden_layer
 
     def get_output_layer(self, x):
-        output_layer = tf.nn.softmax(tf.add(tf.matmul(x, self.weights['output_layer']), self.bias['output_layer']),
-                                     axis=1)
+        output_layer = tf.add(tf.matmul(x, self.weights['output_layer']), self.bias['output_layer'])
         return output_layer
 
     def get_loss(self, y_pred, y_true):
@@ -116,7 +115,7 @@ def demo_wine_classifier():
 def demo_maximum_likelihood_wine_classifier():
     data = utils.read_wine_data()
     classifier = MleWineClassifier(data['training']['features'].iloc[0].shape[0], 4, 3, seed=11)
-    predicted = classifier.predict(data['training'], data['testing'], 0.0001, 1000, 100)
+    predicted = classifier.predict(data['training'], data['testing'], 0.01, 1000, 100)
 
     print("Training Accuracy", accuracy_score(data['training']['labels'], predicted['training']))
     print("Testing Accuracy", accuracy_score(data['testing']['labels'], predicted['testing']))
