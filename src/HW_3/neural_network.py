3fca98139abb9c2ec9d5ec5e3929bb2510b23ca1
gajjar.s <gajjar.s@husky.neu.edu>
2019-02-16 18:05:12 -0500

    done with the custom auto encoder

--- src/HW_3/neural_network.py
+++ src/HW_3/neural_network.py
@@ -125,6 +125,28 @@ class NeuralNetwork:
                 self.weights['hidden_layer'] = hidden_w
 
 
+def demo_auto_encoder():
+    training_features = utils.get_input_for_encoder(8)
+    training_labels = training_features.copy()
+
+    input_dim = training_features[0].shape[0]
+    hidden_layer_dim = 3
+    output_layer_dim = 8
+
+    activation_functions = {
+        'hidden_layer': ActivationFunction.SIGMOID,
+        'output_layer': ActivationFunction.SIGMOID
+    }
+
+    nn = NeuralNetwork(input_dim, hidden_layer_dim, output_layer_dim, activation_functions, Loss.MEAN_SQUARE_ERROR,
+                       123)
+
+    nn.train(training_features, training_labels, 0.2, 2000, 100, 0.001)
+
+    predicted = nn.predict(training_features)
+    print("Predicted ", predicted)
+
+
 def demo_wine_classifier():
     data = utils.read_wine_data()
 
@@ -166,4 +188,5 @@ def demo_wine_classifier():
 
 
 if __name__ == '__main__':
-    demo_wine_classifier()
+    # demo_wine_classifier()
+    demo_auto_encoder()
