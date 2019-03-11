66b9bd53e6be8d30b1333000653d221e0ea9b535
gajjar.s <gajjar.s@husky.neu.edu>
2019-03-11 12:53:56 -0400

    1. minor refactoring 2. added code to plot the ROC

--- src/HW_4/GDA_on_spam.py
+++ src/HW_4/GDA_on_spam.py
@@ -6,9 +6,9 @@ from sklearn.metrics import accuracy_score
 from HW_4 import utils
 
 
-class GDA:
+class SpamGDA:
 
-    def __init__(self, sigma_diagonal_multiplier=1.0) -> None:
+    def __init__(self, sigma_diagonal_multiplier=0.01) -> None:
         super().__init__()
         self.non_spam_mean = None
         self.spam_mean = None
@@ -35,44 +35,33 @@ class GDA:
         temp = features - mean[labels]
         sigma = np.matmul(np.transpose(temp), temp) / features.shape[0]
         di = np.diag_indices(self.d)
-        sigma[di] = sigma[di] * self.sigma_diagonal_multiplier
+        sigma[di] = sigma[di] + self.sigma_diagonal_multiplier
         self.sigma = sigma
 
-    def vectorized_predict(self, features):
+    def get_p_x_given_non_spam(self, features):
         common = 1 / (np.sqrt(np.power(2 * np.pi, self.d) * np.linalg.det(self.sigma)))
         sigma_inverse = np.linalg.pinv(self.sigma)
 
         x_minus_non_spam_mean = features - self.non_spam_mean
         p_x_non_spam = common * np.exp(
             -0.5 * (np.matmul(x_minus_non_spam_mean, sigma_inverse) * x_minus_non_spam_mean).sum(axis=1))
-        p_predict_non_spam = p_x_non_spam * self.p_non_spam
 
+        return p_x_non_spam
+
+    def get_p_x_given_spam(self, features):
+        common = 1 / (np.sqrt(np.power(2 * np.pi, self.d) * np.linalg.det(self.sigma)))
+        sigma_inverse = np.linalg.pinv(self.sigma)
         x_minus_spam_mean = features - self.spam_mean
         p_x_spam = common * np.exp(
             -0.5 * (np.matmul(x_minus_spam_mean, sigma_inverse) * x_minus_spam_mean).sum(axis=1))
-        p_predict_spam = p_x_spam * self.p_spam
 
-        return np.argmax(np.column_stack((p_predict_non_spam, p_predict_spam)), axis=1)
+        return p_x_spam
 
     def predict(self, features):
-        common = 1 / (np.sqrt(np.power(2 * np.pi, self.d) * np.linalg.det(self.sigma)))
-        sigma_inverse = np.linalg.pinv(self.sigma)
+        p_predict_non_spam = self.get_p_x_given_non_spam(features) * self.p_non_spam
+        p_predict_spam = self.get_p_x_given_spam(features) * self.p_spam
 
-        predicted_labels = []
-        for feature in features:
-            x_minus_non_spam_mean = feature - self.non_spam_mean
-            p_x_non_spam = common * np.exp(
-                -0.5 * np.matmul(np.matmul(x_minus_non_spam_mean, sigma_inverse), np.transpose(x_minus_non_spam_mean)))
-            p_predict_non_spam = p_x_non_spam * self.p_non_spam
-
-            x_minus_spam_mean = feature - self.spam_mean
-            p_x_spam = common * np.exp(
-                -0.5 * np.matmul(np.matmul(x_minus_spam_mean, sigma_inverse), np.transpose(x_minus_spam_mean)))
-            p_predict_spam = p_x_spam * self.p_spam
-
-            predicted_labels.append(np.argmax([p_predict_non_spam, p_predict_spam]))
-
-        return predicted_labels
+        return np.column_stack((p_predict_non_spam, p_predict_spam))
 
 
 def demo_gda_on_spam_data():
@@ -82,10 +71,10 @@ def demo_gda_on_spam_data():
     training_accuracy = []
     testing_accuracy = []
     for k_fold_data in k_folds:
-        gda = GDA(1)
+        gda = SpamGDA(0.01)
         gda.train(k_fold_data['training']['features'], k_fold_data['training']['labels'])
-        training_predicted_labels = gda.vectorized_predict(k_fold_data['training']['features'])
-        testing_predicted_labels = gda.vectorized_predict(k_fold_data['testing']['features'])
+        training_predicted_labels = np.argmax(gda.predict(k_fold_data['training']['features']), axis=1)
+        testing_predicted_labels = np.argmax(gda.predict(k_fold_data['testing']['features']), axis=1)
 
         training_accuracy.append(accuracy_score(k_fold_data['training']['labels'], training_predicted_labels))
         testing_accuracy.append(accuracy_score(k_fold_data['testing']['labels'], testing_predicted_labels))
--- src/HW_4/naive_bayes.py
+++ src/HW_4/naive_bayes.py
@@ -1,8 +1,10 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from prettytable import PrettyTable
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, auc
 
 from HW_4 import utils
+from HW_4.GDA_on_spam import SpamGDA
 
 
 class NaiveBayesBernoulli:
@@ -71,7 +73,7 @@ class NaiveBayesBernoulli:
             predicted_non_spam_probs.append(p_non_spam)
             predicted_spam_probs.append(p_spam)
 
-        return np.argmax(np.column_stack((predicted_non_spam_probs, predicted_spam_probs)), axis=1)
+        return np.column_stack((predicted_non_spam_probs, predicted_spam_probs))
 
     @staticmethod
     def convert_continuous_features_to_discrete(features):
@@ -85,6 +87,39 @@ class NaiveBayesBernoulli:
             f_i[f_i > mean[i]] = 1
 
         return features
+
+
+class NaiveBayesGaussian:
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.gda = None
+
+    def train(self, features, labels):
+        self.gda = SpamGDA(0.1)
+        self.gda.train(features, labels)
+        self.gda.sigma[~np.eye(self.gda.sigma.shape[0], dtype=bool)] = 0
+
+    def predict(self, features):
+        return self.gda.predict(features)
+
+
+class NaiveBayesFourBins:
+
+    def __init__(self, seed, dimension, k) -> None:
+        super().__init__()
+        self.dimension = dimension
+        self.k = k
+        self.seed = seed
+        self.prior_p_non_spam = None
+        self.prior_p_spam = None
+
+    def train(self, features, labels):
+        non_spam_count = labels[labels == 0].shape[0]
+        spam_count = labels[labels == 1].shape[0]
+
+        self.prior_p_non_spam = non_spam_count / features.shape[0]
+        self.prior_p_spam = spam_count / features.shape[0]
 
 
 def print_info_table(info_table):
@@ -102,38 +137,62 @@ def print_info_table(info_table):
     print(table)
 
 
-def demo_naive_bayes_with_bernoulli_features():
-    data = utils.get_spam_data()
-    data['features'] = NaiveBayesBernoulli.convert_continuous_features_to_discrete(data['features'])
+def plot_roc_curve(testing_true_labels, testing_predictions):
+    testing_predictions = testing_predictions.copy()
 
-    k_folds = utils.k_fold_split(10, data, seed=1, shuffle=True)
+    log_odds = np.log(testing_predictions[:, 1]) - np.log(testing_predictions[:, 0])
+    sorted_indices = np.argsort(log_odds)
+
+    fpr, tpr, thresholds = roc_curve(testing_true_labels[sorted_indices], np.sort(log_odds))
+    area_under_curve = auc(fpr, tpr)
+
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % area_under_curve)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.1])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+def demo_classifier(data, classifier, classifier_name):
+    k_folds = utils.k_fold_split(10, data, seed=34928731, shuffle=True)
 
     training_accuracy = []
     testing_accuracy = []
 
     info_table = []
 
+    print("+" * 20, classifier_name, "+" * 20)
     i = 1
     for k_fold_data in k_folds:
-        naive_bayes = NaiveBayesBernoulli(1, data['features'].shape[1], 1)
-        naive_bayes.train(k_fold_data['training']['features'], k_fold_data['training']['labels'])
+        classifier.train(k_fold_data['training']['features'], k_fold_data['training']['labels'])
 
-        training_predicted_labels = naive_bayes.predict(k_fold_data['training']['features'])
-        testing_predicted_labels = naive_bayes.predict(k_fold_data['testing']['features'])
+        training_predictions = classifier.predict(k_fold_data['training']['features'])
+        training_prediction_labels = np.argmax(training_predictions, axis=1)
 
-        training_accuracy.append(accuracy_score(k_fold_data['training']['labels'], training_predicted_labels))
+        testing_predictions = classifier.predict(k_fold_data['testing']['features'])
+        testing_prediction_labels = np.argmax(testing_predictions, axis=1)
+
+        training_accuracy.append(
+            accuracy_score(k_fold_data['training']['labels'], training_prediction_labels))
 
         testing_true_labels = k_fold_data['testing']['labels']
-        testing_accuracy.append(accuracy_score(testing_true_labels, testing_predicted_labels))
+        testing_accuracy.append(accuracy_score(testing_true_labels, testing_prediction_labels))
 
-        tn, fp, fn, tp = confusion_matrix(testing_true_labels, testing_predicted_labels).ravel()
+        tn, fp, fn, tp = confusion_matrix(testing_true_labels, testing_prediction_labels).ravel()
         fp_rate = fp / testing_true_labels.shape[0]
         fn_rate = fn / testing_true_labels.shape[0]
         error_rate = fp_rate + fn_rate
         info_table.append(["{}".format(i), fp_rate, fn_rate, error_rate])
 
-        # if i == 1:
-        # utils.plot_roc_curve(testing_true_labels, testing_predicted_labels)
+        if i == 1:
+            plot_roc_curve(testing_true_labels, testing_predictions)
 
         i += 1
 
@@ -141,7 +200,21 @@ def demo_naive_bayes_with_bernoulli_features():
 
     print("Training accuracy: ", np.mean(training_accuracy))
     print("Testing accuracy: ", np.mean(testing_accuracy))
+    print("+" * 80)
+    print()
+
+
+def demo_naive_bayes_with_bernoulli_features():
+    data = utils.get_spam_data()
+    data['features'] = NaiveBayesBernoulli.convert_continuous_features_to_discrete(data['features'])
+    demo_classifier(data, NaiveBayesBernoulli(1, data['features'].shape[1], 1), "Naive Bayes Bernoulli")
+
+
+def demo_naive_bayes_gaussian_features():
+    data = utils.get_spam_data()
+    demo_classifier(data, NaiveBayesGaussian(), "Naive Bayes Gaussian")
 
 
 if __name__ == '__main__':
     demo_naive_bayes_with_bernoulli_features()
+    demo_naive_bayes_gaussian_features()
