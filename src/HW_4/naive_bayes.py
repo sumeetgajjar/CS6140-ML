f507225c7fb9b937c267233520fc587600b71b90
gajjar.s <gajjar.s@husky.neu.edu>
2019-03-11 14:50:15 -0400

    1. added code for naive bayes four and nine bins

--- src/HW_4/naive_bayes.py
+++ src/HW_4/naive_bayes.py
@@ -104,15 +104,18 @@ class NaiveBayesGaussian:
         return self.gda.predict(features)
 
 
-class NaiveBayesFourBins:
+class NaiveBayesBins:
 
-    def __init__(self, seed, dimension, k) -> None:
+    def __init__(self, seed, bins, dimension, k) -> None:
         super().__init__()
         self.dimension = dimension
+        self.bins = bins
         self.k = k
         self.seed = seed
         self.prior_p_non_spam = None
         self.prior_p_spam = None
+        self.non_spam_bin_prob = None
+        self.spam_bin_prob = None
 
     def train(self, features, labels):
         non_spam_count = labels[labels == 0].shape[0]
@@ -120,6 +123,74 @@ class NaiveBayesFourBins:
 
         self.prior_p_non_spam = non_spam_count / features.shape[0]
         self.prior_p_spam = spam_count / features.shape[0]
+
+        non_spam_bin_prob = []
+        spam_bin_prob = []
+
+        for i in range(self.dimension):
+            non_spam_prob = []
+            spam_prob = []
+            for j in range(self.bins):
+                f_i = features[:, i][labels == 0]
+                i_bin_j_non_spam_count = f_i[f_i == j].shape[0]
+                p_i_bin_j_non_spam = (i_bin_j_non_spam_count + self.k) / (non_spam_count + (2 * self.k))
+                non_spam_prob.append(p_i_bin_j_non_spam)
+
+                f_i = features[:, i][labels == 1]
+                i_bin_j_spam_count = f_i[f_i == j].shape[0]
+                p_i_bin_j_spam = (i_bin_j_spam_count + self.k) / (spam_count + (2 * self.k))
+                spam_prob.append(p_i_bin_j_spam)
+
+            non_spam_bin_prob.append(non_spam_prob)
+            spam_bin_prob.append(spam_prob)
+
+        self.non_spam_bin_prob = np.array(non_spam_bin_prob)
+        self.spam_bin_prob = np.array(spam_bin_prob)
+
+    def predict(self, features):
+
+        predicted_non_spam_probs = []
+        predicted_spam_probs = []
+
+        for f in features:
+
+            p_non_spam = 1
+            p_spam = 1
+            for j in range(self.bins):
+                p_non_spam *= np.product(self.non_spam_bin_prob[f == j, j])
+                p_spam *= np.product(self.spam_bin_prob[f == j, j])
+
+            p_non_spam *= self.prior_p_non_spam
+            p_spam *= self.prior_p_spam
+
+            predicted_non_spam_probs.append(p_non_spam)
+            predicted_spam_probs.append(p_spam)
+
+        return np.column_stack((predicted_non_spam_probs, predicted_spam_probs))
+
+    @staticmethod
+    def convert_continuous_features_to_four_bins(features, labels):
+        features = features.copy()
+        mean = features.mean(axis=0)
+        non_spam_mean = features[labels == 0].mean(axis=0)
+        spam_mean = features[labels == 1].mean(axis=1)
+
+        dimension = features.shape[1]
+        for i in range(dimension):
+            temp = np.sort([mean[i], non_spam_mean[i], spam_mean[i]])
+            features[:, i] = np.digitize(features[:, i], temp)
+
+        return features
+
+    @staticmethod
+    def convert_continuous_features_to_n_bins(features, number_of_bins):
+        features = features.copy()
+        dimension = features.shape[1]
+        for i in range(dimension):
+            bins = np.linspace(np.min(features[:, i]), np.max(features[:, i]), number_of_bins)
+            features[:, i] = np.digitize(features[:, i], bins)
+
+        return features
 
 
 def print_info_table(info_table):
@@ -137,7 +208,7 @@ def print_info_table(info_table):
     print(table)
 
 
-def plot_roc_curve(testing_true_labels, testing_predictions):
+def plot_roc_curve(testing_true_labels, testing_predictions, title):
     testing_predictions = testing_predictions.copy()
 
     log_odds = np.log(testing_predictions[:, 1]) - np.log(testing_predictions[:, 0])
@@ -155,7 +226,7 @@ def plot_roc_curve(testing_true_labels, testing_predictions):
     plt.ylim([0.0, 1.1])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic example')
+    plt.title(title)
     plt.legend(loc="lower right")
     plt.show()
 
@@ -192,7 +263,7 @@ def demo_classifier(data, classifier, classifier_name):
         info_table.append(["{}".format(i), fp_rate, fn_rate, error_rate])
 
         if i == 1:
-            plot_roc_curve(testing_true_labels, testing_predictions)
+            plot_roc_curve(testing_true_labels, testing_predictions, classifier_name)
 
         i += 1
 
@@ -215,6 +286,20 @@ def demo_naive_bayes_gaussian_features():
     demo_classifier(data, NaiveBayesGaussian(), "Naive Bayes Gaussian")
 
 
+def demo_naive_bayes_four_bin():
+    data = utils.get_spam_data()
+    data['features'] = NaiveBayesBins.convert_continuous_features_to_four_bins(data['features'], data['labels'])
+    demo_classifier(data, NaiveBayesBins(1, 4, data['features'].shape[1], 1), "Naive Bayes 4 bins")
+
+
+def demo_naive_bayes_nine_bins():
+    data = utils.get_spam_data()
+    data['features'] = NaiveBayesBins.convert_continuous_features_to_n_bins(data['features'], 9)
+    demo_classifier(data, NaiveBayesBins(1, 9, data['features'].shape[1], 1), "Naive Bayes 9 bins")
+
+
 if __name__ == '__main__':
     demo_naive_bayes_with_bernoulli_features()
     demo_naive_bayes_gaussian_features()
+    demo_naive_bayes_four_bin()
+    demo_naive_bayes_nine_bins()
