2af11887b0c6b8a6687508d4b24a57203fe340af
gajjar.s <gajjar.s@husky.neu.edu>
2019-03-16 16:31:20 -0400

    1. added files for AdaBoost

---
+++ src/HW_5/AdaBoost.py
@@ -0,0 +1,158 @@
+from enum import Enum
+
+import numpy as np
+from sklearn.metrics import accuracy_score
+
+from HW_5 import utils
+
+
+class Predictor:
+
+    def __init__(self, feature_index, threshold) -> None:
+        super().__init__()
+        self.feature_index = feature_index
+        self.threshold = threshold
+
+    def predict(self, features):
+        y_predicted = np.ones(features.shape[0])
+        y_predicted[features[:, self.feature_index] <= self.threshold] = -1
+        return y_predicted
+
+
+class DecisionStumpType(Enum):
+    OPTIMAL = 1
+    RANDOM = 2
+
+
+class DecisionStump:
+
+    def __init__(self, decision_stump_type, features, labels) -> None:
+        super().__init__()
+        self.decision_stump_type = decision_stump_type
+        self.features = features
+        self.labels = labels
+        self.predictor = None
+        self.__initialize()
+
+    def __initialize(self):
+        if self.decision_stump_type is DecisionStumpType.OPTIMAL:
+            self.find_optimal_decision_stump()
+        elif self.decision_stump_type is DecisionStumpType.RANDOM:
+            self.find_random_decision_stump()
+        else:
+            raise Exception("Invalid Decision Stump Type")
+
+    def __get_unique_thresholds_for_feature(self, feature_index):
+        all_values = self.features[:, feature_index]
+        return np.array([np.unique(all_values), np.min(all_values) - 1, np.max(all_values) + 1])
+
+    def find_optimal_decision_stump(self):
+        min_error = 1
+        best_predictor = None
+
+        for i in range(self.features.shape[1]):
+            unique_thresholds = self.__get_unique_thresholds_for_feature(i)
+            sorted_unique_thresholds = np.sort(unique_thresholds)
+
+            for threshold in sorted_unique_thresholds:
+                predictor = Predictor(i, threshold)
+                y_predicted = predictor.predict(self.features)
+                error = 1 - accuracy_score(self.labels, y_predicted)
+                error = abs(0.5 - error)
+
+                if error < min_error:
+                    min_error = error
+                    best_predictor = predictor
+
+        self.predictor = best_predictor
+
+    def find_random_decision_stump(self):
+        feature_index = np.random.randint(0, self.features.shape[1])
+
+        unique_thresholds = self.__get_unique_thresholds_for_feature(feature_index)
+        feature_threshold = unique_thresholds[np.random.randint(0, unique_thresholds.shape[0])]
+
+        self.predictor = Predictor(feature_index, feature_threshold)
+
+    def predict(self, features):
+        return self.predictor.predict(features)
+
+
+class AdaBoost:
+
+    def __init__(self, decision_stump_type) -> None:
+        super().__init__()
+        self.decision_stump_type = decision_stump_type
+        self.alpha = None
+        self.weak_learners = []
+        self.local_round_error = []
+        self.global_error = []
+
+    def get_weak_learner(self, features, true_labels):
+        return DecisionStump(self.decision_stump_type, features, true_labels)
+
+    def train(self, training_features, training_labels, no_of_weak_learners):
+        d_t = np.repeat(1 / training_features.shape[0], training_features.shape[0])
+        training_features = training_features.copy()
+
+        alpha = []
+        global_error = 0
+        for i in range(no_of_weak_learners):
+            for i in range(training_features.shape[1]):
+                training_features[:, i] = training_features[:, i] * d_t
+
+            weak_learner = self.get_weak_learner(training_features, training_labels)
+            self.weak_learners.append(weak_learner)
+
+            predicted_labels = weak_learner.predict(training_features)
+            epsilon_error = d_t[training_labels != predicted_labels].sum()
+
+            alpha_t = 0.5 * (np.log(1 - epsilon_error) - np.log(epsilon_error))
+            alpha.append(alpha_t)
+
+            d_t = d_t * (np.exp(-alpha_t * training_labels * predicted_labels))
+            d_t = d_t / d_t.sum()
+
+            global_error += np.sum(training_labels - (predicted_labels * alpha_t))
+
+
+        self.alpha = np.array(alpha)
+
+    def predict(self, features):
+        h_t = []
+        for t in range(self.alpha.shape[0]):
+            predicted_labels = self.weak_learners[t].predict(features)
+            h_t.append(predicted_labels)
+
+        return np.sum(np.transpose(np.array(h_t)) * self.alpha, axis=1)
+
+
+def demo_ada_boost_with_optimal_decision_stump():
+    data = utils.get_spam_data_for_ada_boost()
+    k = 10
+    folds = utils.k_fold_split(k, data, seed=11, shuffle=True)
+
+    training_accuracy = []
+    testing_accuracy = []
+    for data in folds:
+        training_features = data['features']['training']
+        training_labels = data['labels']['training']
+        testing_features = data['features']['testing']
+        testing_labels = data['labels']['testing']
+
+        ada_boost = AdaBoost(DecisionStumpType.OPTIMAL)
+        ada_boost.train(training_features, training_labels, 10)
+
+        training_predictions = ada_boost.predict(training_features)
+        training_accuracy.append(accuracy_score(training_labels, training_predictions))
+
+        testing_predictions = ada_boost.predict(testing_features)
+        testing_accuracy.append(accuracy_score(testing_labels, testing_predictions))
+
+    print("Training Accuracy:", np.mean(training_accuracy))
+    print("Testing Accuracy:", np.mean(testing_accuracy))
+
+
+if __name__ == '__main__':
+    np.random.seed(11)
+    demo_ada_boost_with_optimal_decision_stump()
---
+++ src/HW_5/utils.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pandas as pd
+
+ROOT = '../../'
+
+
+def get_spam_data():
+    data = pd.read_csv('%sdata/spam-email/spambase.data' % ROOT, header=None)
+    features = np.array(data.iloc[:, :-1])
+    labels = np.array(data.iloc[:, -1])
+
+    if features.shape[0] != labels.shape[0]:
+        raise Exception("Mismatch in Feature Tuples(%d) and Label Tuples(%d)" % (features.size, labels.size))
+
+    return {
+        'features': features,
+        'labels': labels
+    }
+
+
+def get_spam_data_for_ada_boost():
+    data = get_spam_data()
+    labels = data['labels']
+    labels[labels == 0] = -1
+    data['labels'] = labels
+
+
+def k_fold_split(k, data, seed=11, shuffle=False):
+    sample_size = data['features'].shape[0]
+
+    indices = np.arange(0, sample_size)
+
+    if shuffle:
+        np.random.seed(seed)
+        np.random.shuffle(indices)
+
+    folds = np.array_split(indices, k)
+    testing_fold_index = 0
+    final_data = []
+
+    for i in range(0, k):
+        training_folds = [folds[j] for j in range(0, k) if j != testing_fold_index]
+        training_data_indices = np.concatenate(training_folds)
+        training_data_features = data['features'][training_data_indices]
+        training_data_labels = data['labels'][training_data_indices]
+
+        testing_data_indices = folds[testing_fold_index]
+        testing_data_features = data['features'][testing_data_indices]
+        testing_data_labels = data['labels'][testing_data_indices]
+
+        temp = {
+            'training': {
+                'features': training_data_features,
+                'labels': training_data_labels
+            },
+            'testing': {
+                'features': testing_data_features,
+                'labels': testing_data_labels
+            }
+        }
+
+        final_data.append(temp)
+        testing_fold_index += 1
+
+    return final_data
+
+
+def normalize_data_using_zero_mean_unit_variance(feature_vectors):
+    total_features = len(feature_vectors[0])
+    i = 0
+    while i < total_features:
+        feature_values = feature_vectors[:, i]
+        mean = feature_values.mean()
+        std = feature_values.std()
+        normalized_values = (feature_values - mean) / std
+        feature_vectors[:, i] = normalized_values
+
+        i += 1
+
+    return feature_vectors
+
+
+def normalize_data_using_shift_and_scale(feature_vectors):
+    total_features = len(feature_vectors[0])
+    i = 0
+    while i < total_features:
+        feature_values = feature_vectors[:, i]
+        f_min = feature_values.min()
+        f_max = feature_values.max()
+        normalized_values = (feature_values - f_min) / (f_max - f_min)
+        feature_vectors[:, i] = normalized_values
+
+        i += 1
+
+    return feature_vectors
