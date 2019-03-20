0dc6e300e16edca77d3c60b1f8e79b60ee7b358f
gajjar.s <gajjar.s@husky.neu.edu>
2019-03-20 15:59:50 -0400

    refactored the bagging code

--- src/HW_5/bagging.py
+++ src/HW_5/bagging.py
@@ -1,14 +1,41 @@
 import numpy as np
-from matplotlib import pylab as plt
 from sklearn.metrics import accuracy_score
 
 from HW_1 import DecisionClassificationTree
 from HW_5 import utils
 
 
-def random_sample_data(features, labels, N):
-    random_indexes = np.random.random_integers(0, features.shape[0] - 1, N)
-    return features[random_indexes], labels[random_indexes]
+class Bagging:
+
+    def __init__(self, bags) -> None:
+        super().__init__()
+        self.bags = bags
+        self.classifiers = []
+
+    @staticmethod
+    def __random_sample_data(features, labels, N):
+        random_indexes = np.random.random_integers(0, features.shape[0] - 1, N)
+        return features[random_indexes], labels[random_indexes]
+
+    def train(self, features, labels, N):
+
+        for t in range(self.bags):
+            sampled_training_features, sampled_training_labels = self.__random_sample_data(features, labels, N)
+
+            tree = DecisionClassificationTree.create_tree(sampled_training_features, sampled_training_labels, 2, 4)
+            self.classifiers.append(tree)
+
+            current_training_predictions = tree.predict(sampled_training_features)
+            current_training_acc = accuracy_score(sampled_training_labels, current_training_predictions)
+
+            print("Bag:{}, Training Acc:{}".format(t + 1, current_training_acc))
+
+    def predict(self, features):
+        predicted = []
+        for classifier in self.classifiers:
+            predicted.append(classifier.predict(features))
+
+        return np.mean(np.array(predicted), axis=0)
 
 
 def demo_bagging_on_spam():
@@ -20,38 +47,22 @@ def demo_bagging_on_spam():
     training_labels = folds[0]['training']['labels']
     testing_labels = folds[0]['testing']['labels']
 
-    training_acc = []
-    testing_acc = []
-    classifiers = []
-    testing_predictions = []
-    for t in range(1, bags + 1):
-        sampled_training_features, sampled_training_labels = random_sample_data(training_features, training_labels, N)
+    classifier = Bagging(bags)
+    classifier.train(training_features, training_labels, N)
 
-        tree = DecisionClassificationTree.create_tree(sampled_training_features, sampled_training_labels, 2, 4)
-        classifiers.append(tree)
+    acc = accuracy_score(training_labels, classifier.classifiers[0].predict(training_features))
+    print("Training Accuracy without Bagging:", acc)
 
-        current_training_predictions = tree.predict(sampled_training_features)
-        current_training_acc = accuracy_score(sampled_training_labels, current_training_predictions)
-        training_acc.append(current_training_acc)
+    training_predictions = classifier.predict(training_features)
+    acc, labels = utils.convert_predictions_to_labels(training_labels, training_predictions)
+    print("Training Accuracy with Bagging:", acc)
 
-        current_testing_predictions = tree.predict(testing_features)
-        current_testing_accuracy = accuracy_score(testing_labels, current_testing_predictions)
-        testing_acc.append(current_testing_accuracy)
-        testing_predictions.append(current_testing_predictions)
+    acc = accuracy_score(testing_labels, classifier.classifiers[0].predict(testing_features))
+    print("Testing Accuracy without Bagging:", acc)
 
-        print("Bag:{}, Training Acc:{}, Testing Acc:{}".format(t, current_training_acc, current_testing_accuracy))
-
-    acc = accuracy_score(testing_labels, testing_predictions[0])
-    print("Testing Accuracy without Bagging: ", acc)
-
-    testing_predictions = np.mean(np.array(testing_predictions), axis=0)
+    testing_predictions = classifier.predict(testing_features)
     acc, labels = utils.convert_predictions_to_labels(testing_labels, testing_predictions, 0)
-    print("Testing Accuracy with Bagging: ", acc)
-
-    plt.plot(range(bags), training_acc, label="Training Acc", c="Orange")
-    plt.plot(range(bags), testing_acc, label="Testing Acc", c="Blue")
-    plt.legend(loc=4)
-    plt.show()
+    print("Testing Accuracy with Bagging:", acc)
 
     utils.plot_roc_curve(testing_labels, testing_predictions)
 
