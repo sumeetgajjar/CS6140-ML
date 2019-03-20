16e95d5511fc569b5487fec706371de4075c5b31
gajjar.s <gajjar.s@husky.neu.edu>
2019-03-20 10:15:58 -0400

    bug fixes in crx and vote ada boost

--- src/HW_5/uci_adaboost.py
+++ src/HW_5/uci_adaboost.py
@@ -23,13 +23,14 @@ def fill_missing_data(data, config):
 
 
 def demo_uci_crx_optimal_decision_stump():
+    print("+" * 40, "CRX", "+" * 40)
     parser = UciDataParser('crx')
     data = parser.parse_data()
     config = parser.config
     data = fill_missing_data(data, config)
 
     k = 10
-    folds = utils.k_fold_split(k, data, seed=11, shuffle=True)
+    folds = utils.k_fold_split(k, data, seed=1, shuffle=True)
 
     for data in folds[:1]:
         training_features = data['training']['features']
@@ -37,15 +38,22 @@ def demo_uci_crx_optimal_decision_stump():
         testing_features = data['testing']['features']
         testing_labels = data['testing']['labels']
 
-        ada_boost = AdaBoost(DecisionStumpType.OPTIMAL)
-        ada_boost.train(training_features, training_labels, testing_features, testing_labels, 100)
+        ada_boost = AdaBoost(DecisionStumpType.RANDOM)
+        ada_boost.train(training_features, training_labels, testing_features, testing_labels, 200, 20)
 
         testing_predictions = ada_boost.predict(testing_features)
         ada_boost.plot_metrics()
         utils.plot_roc_curve(testing_labels, testing_predictions)
 
+        acc, labels = utils.convert_predictions_to_labels(testing_labels, testing_predictions)
+        print("Testing Accuracy: ", acc)
+
+    print("+" * 40, "CRX", "+" * 40)
+    print()
+
 
 def demo_uci_vote_optimal_decision_stump():
+    print("+" * 40, "VOTE", "+" * 40)
     parser = UciDataParser('vote')
     data = parser.parse_data()
     config = parser.config
@@ -61,14 +69,48 @@ def demo_uci_vote_optimal_decision_stump():
         testing_labels = data['testing']['labels']
 
         ada_boost = AdaBoost(DecisionStumpType.OPTIMAL)
-        ada_boost.train(training_features, training_labels, testing_features, testing_labels, 20)
+        ada_boost.train(training_features, training_labels, testing_features, testing_labels, 12, 2)
 
         testing_predictions = ada_boost.predict(testing_features)
         ada_boost.plot_metrics()
         utils.plot_roc_curve(testing_labels, testing_predictions)
 
+        acc, labels = utils.convert_predictions_to_labels(testing_labels, testing_predictions)
+        print("Testing Accuracy: ", acc)
+
+    print("+" * 40, "VOTE", "+" * 40)
+    print()
+
+
+def demo_uci_crx_optimal_decision_stump_for_c_percent_data():
+    parser = UciDataParser('vote')
+    data = parser.parse_data()
+    config = parser.config
+    data = fill_missing_data(data, config)
+
+    k = 10
+    folds = utils.k_fold_split(k, data, seed=11, shuffle=True)
+
+    for data in folds[:1]:
+        training_features = data['training']['features']
+        training_labels = data['training']['labels']
+        testing_features = data['testing']['features']
+        testing_labels = data['testing']['labels']
+
+        for c in [5, 10, 15, 20, 30, 50, 80]:
+            data_points = np.ceil((c / 100) * training_features.shape[0])
+            training_features = training_features[:data_points]
+            training_labels = training_labels[:data_points]
+
+            ada_boost = AdaBoost(DecisionStumpType.OPTIMAL)
+            ada_boost.train(training_features, training_labels, testing_features, testing_labels, 20)
+
+            testing_predictions = ada_boost.predict(testing_features)
+            ada_boost.plot_metrics()
+            utils.plot_roc_curve(testing_labels, testing_predictions)
+
 
 if __name__ == '__main__':
     np.random.seed(2)
-    # demo_uci_crx_optimal_decision_stump()
+    demo_uci_crx_optimal_decision_stump()
     demo_uci_vote_optimal_decision_stump()
