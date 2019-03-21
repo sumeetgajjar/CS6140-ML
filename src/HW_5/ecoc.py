fa07f3c8e63c6f7bb4c21df066e582966ff5d3f8
gajjar.s <gajjar.s@husky.neu.edu>
2019-03-20 23:27:13 -0400

    added ecoc file in the

---
+++ src/HW_5/ecoc.py
@@ -0,0 +1,18 @@
+class ECOC:
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
+
+
+def demo_ecoc_on_8_news_group_data():
+    pass
+
+
+if __name__ == '__main__':
+    demo_ecoc_on_8_news_group_data()
