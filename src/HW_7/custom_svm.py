067881b2620930d5b5acd221ab555a88e2b9d43f
gajjar.s <gajjar.s@husky.neu.edu>
2019-04-08 12:34:38 -0400

    1. added methods to compute E, L and H, n

--- src/HW_7/custom_svm.py
+++ src/HW_7/custom_svm.py
@@ -19,6 +19,27 @@ class SVM:
         summation = (self.alpha * self.labels * dot_product).sum()
         return summation + self.b
 
+    @staticmethod
+    def __select_j_randomly(i, m):
+        while True:
+            j = np.random.randint(0, m)
+            if i != j:
+                return j
+
+    def __calculate_E(self, x, y):
+        return self.__f_x(x) - y
+
+    def __compute_L_H(self, y_i, alpha_i, y_j, alpha_j):
+
+        if y_i != y_j:
+            return np.max(0, alpha_j - alpha_i), np.min(self.C, self.C + alpha_j - alpha_i)
+        else:
+            return np.max(0, alpha_i + alpha_j - self.C), np.min(self.C, alpha_i + alpha_j)
+
+    def __compute_n(self, x_i, x_j):
+        return 2 * np.dot(x_i, x_j) - np.dot(x_i, x_i) - np.dot(x_j, x_j)
+
+
     def train(self, features, labels):
         m = features.shape[0]
 
@@ -29,7 +50,21 @@ class SVM:
         while passes <= self.max_passes:
             num_alpha_changed = 0
             for i in range(m):
-                E_i =
+                x_i, y_i, alpha_i = features[i], labels[i], self.alpha[i]
+
+                E_i = self.__calculate_E(x_i, y_i)
+                temp = E_i * y_i
+                if (temp < -self.tol and alpha_i < self.C) or (temp > self.tol and alpha_i > self.C):
+                    j = self.__select_j_randomly(i, m)
+                    x_j, y_j, alpha_j = features[j], labels[j], self.alpha[j]
+                    E_j = self.__calculate_E(x_j, y_j)
+                    alpha_i_old, alpha_j_old = alpha_i, alpha_j
+                    L, H = self.__compute_L_H(y_i, alpha_i, y_j, alpha_j)
+
+                    if L == H:
+                        continue
+
+                    n = self.__compute_n()
 
             if self.display and (passes == 1 or passes % self.max_passes == 0):
                 print("Step=>{}".format(passes))
