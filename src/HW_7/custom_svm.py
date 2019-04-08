cd99ba4fdbe7c298df29bc79bfd1f9ff301c6992
gajjar.s <gajjar.s@husky.neu.edu>
2019-04-08 12:58:10 -0400

    completed the algo for the smo

--- src/HW_7/custom_svm.py
+++ src/HW_7/custom_svm.py
@@ -19,6 +19,9 @@ class SVM:
         summation = (self.alpha * self.labels * dot_product).sum()
         return summation + self.b
 
+    def __calculate_E(self, x, y):
+        return self.__f_x(x) - y
+
     @staticmethod
     def __select_j_randomly(i, m):
         while True:
@@ -26,19 +29,43 @@ class SVM:
             if i != j:
                 return j
 
-    def __calculate_E(self, x, y):
-        return self.__f_x(x) - y
-
-    def __compute_L_H(self, y_i, alpha_i, y_j, alpha_j):
+    @staticmethod
+    def __compute_L_H(y_i, alpha_i, y_j, alpha_j, C):
 
         if y_i != y_j:
-            return np.max(0, alpha_j - alpha_i), np.min(self.C, self.C + alpha_j - alpha_i)
+            return np.max(0, alpha_j - alpha_i), np.min(C, C + alpha_j - alpha_i)
         else:
-            return np.max(0, alpha_i + alpha_j - self.C), np.min(self.C, alpha_i + alpha_j)
+            return np.max(0, alpha_i + alpha_j - C), np.min(C, alpha_i + alpha_j)
 
-    def __compute_n(self, x_i, x_j):
+    @staticmethod
+    def __compute_n(x_i, x_j):
         return 2 * np.dot(x_i, x_j) - np.dot(x_i, x_i) - np.dot(x_j, x_j)
 
+    @staticmethod
+    def __compute_and_clip_alpha_j(alpha_j, y_j, E_i, E_j, n, L, H):
+        alpha_j = alpha_j - (y_j * ((E_i - E_j) / n))
+
+        if alpha_j > H:
+            return H
+        elif L <= alpha_j <= H:
+            return alpha_j
+        else:
+            return L
+
+    @staticmethod
+    def __compute_alpha_i(alpha_i, y_i, y_j, alpha_j_old, alpha_j):
+        return alpha_i + (y_i * y_j * (alpha_j_old - alpha_j))
+
+    @staticmethod
+    def __compute_b1_b2(x_i, y_i, E_i, alpha_i, alpha_i_old, x_j, y_j, E_j, alpha_j, alpha_j_old, b):
+        x_i_dot = np.dot(x_i, x_i)
+        x_j_dot = np.dot(x_j, x_j)
+        x_i_x_j_dot = np.dot(x_i, x_j)
+
+        b1 = b - E_i - (y_i * (alpha_i - alpha_i_old) * x_i_dot) - (y_j * (alpha_j - alpha_j_old) * x_i_x_j_dot)
+        b2 = b - E_j - (y_i * (alpha_i - alpha_i_old) * x_i_x_j_dot) - (y_j * (alpha_j - alpha_j_old) * x_j_dot)
+
+        return b1, b2
 
     def train(self, features, labels):
         m = features.shape[0]
@@ -58,18 +85,45 @@ class SVM:
                     j = self.__select_j_randomly(i, m)
                     x_j, y_j, alpha_j = features[j], labels[j], self.alpha[j]
                     E_j = self.__calculate_E(x_j, y_j)
-                    alpha_i_old, alpha_j_old = alpha_i, alpha_j
-                    L, H = self.__compute_L_H(y_i, alpha_i, y_j, alpha_j)
 
+                    alpha_i_old, alpha_j_old = alpha_i, alpha_j
+
+                    L, H = self.__compute_L_H(y_i, alpha_i, y_j, alpha_j, self.C)
                     if L == H:
                         continue
 
-                    n = self.__compute_n()
+                    n = self.__compute_n(x_i, x_j)
+                    if n >= 0:
+                        continue
+
+                    alpha_j = self.__compute_and_clip_alpha_j(alpha_j, y_j, E_i, E_j, n, L, H)
+
+                    if np.abs(alpha_j - alpha_j_old) < 1e-05:
+                        continue
+
+                    alpha_i = self.__compute_alpha_i(alpha_i, y_i, y_j, alpha_j_old, alpha_j)
+                    b1, b2 = self.__compute_b1_b2(x_i, y_i, E_i, alpha_i, alpha_i_old,
+                                                  x_j, y_j, E_j, alpha_j, alpha_j_old, self.b)
+
+                    if 0 < alpha_i < self.C:
+                        self.b = b1
+                    elif 0 < alpha_j < self.C:
+                        self.b = b2
+                    else:
+                        self.b = (b1 + b2) / 2
+
+                    self.alpha[i] = alpha_i
+                    self.alpha[j] = alpha_j
+
+                    num_alpha_changed += 1
+
+            if num_alpha_changed == 0:
+                passes += 1
+            else:
+                passes = 0
 
             if self.display and (passes == 1 or passes % self.max_passes == 0):
                 print("Step=>{}".format(passes))
-
-            passes += 1
 
     def predict(self, features):
         pass
