7b4c93c3f7c95effe2d25286151cbb5c46cd0f02
gajjar.s <gajjar.s@husky.neu.edu>
2019-04-08 14:33:12 -0400

    added a method to plot the 2d data

--- src/HW_7/custom_svm_demo.py
+++ src/HW_7/custom_svm_demo.py
@@ -4,9 +4,7 @@ from pandas import DataFrame
 from sklearn.datasets import make_blobs
 
 
-def demo_custom_svm_on_2d_data():
-    X, y = make_blobs(n_samples=100, centers=2, n_features=2)
-
+def display_2d_data(X, y):
     df = DataFrame(dict(x=X[:, 0], y=X[:, 1], label=y))
     colors = {0: 'red', 1: 'blue'}
     fig, ax = pyplot.subplots()
@@ -14,6 +12,12 @@ def demo_custom_svm_on_2d_data():
     for key, group in grouped:
         group.plot(ax=ax, kind='scatter', x='x', y='y', label=key, color=colors[key])
     pyplot.show()
+
+
+def demo_custom_svm_on_2d_data():
+    X, y = make_blobs(n_samples=100, centers=2, n_features=2)
+
+    display_2d_data(X, y)
 
 
 if __name__ == '__main__':
