c8c3b52c20d7bbc88c29d3e88df047d7632830fc
gajjar.s <gajjar.s@husky.neu.edu>
2019-04-12 22:38:10 -0400

    1. normalizing the data before running knn 2. added method to normalize data in  utils

--- src/HW_8/demo_knn.py
+++ src/HW_8/demo_knn.py
@@ -27,16 +27,20 @@ def demo_wrapper(data, kernel, mode=KNNMode.K_POINTS, k_list=(1, 3, 7), n_jobs=1
 def demo_k_points_knn_on_spam_base_data():
     print("+" * 40, "K Points KNN on Spambase Data with Euclidean Kernel", "+" * 40)
     data = utils.get_spam_data()
+    data['features'] = utils.normalize_data_using_zero_mean_unit_variance(data['features'])
     data = utils.k_fold_split(10, data, 11, shuffle=True)[0]
-    demo_wrapper(data, Kernel.EUCLIDEAN)
+
+    demo_wrapper(data, Kernel.EUCLIDEAN, k_list=[1, 2, 3])
     print("+" * 40, "K Points KNN on Spambase Data with Euclidean Kernel", "+" * 40)
 
 
 def demo_window_knn_on_spam_base_data():
     print("+" * 40, "Window KNN on Spambase Data with Euclidean Kernel", "+" * 40)
     data = utils.get_spam_data()
+    data['features'] = utils.normalize_data_using_zero_mean_unit_variance(data['features'])
     data = utils.k_fold_split(10, data, 11, shuffle=True)[0]
-    demo_wrapper(data, Kernel.EUCLIDEAN, mode=KNNMode.RADIUS, k_list=[1, 2, 4, 8, 16, 32, 512])
+
+    demo_wrapper(data, Kernel.EUCLIDEAN, mode=KNNMode.RADIUS, k_list=[1, 2.5])
     print("+" * 40, "Window KNN on Spambase Data with Euclidean Kernel", "+" * 40)
 
 
@@ -52,8 +56,8 @@ def demo_k_points_knn_on_mnist_data():
 def demo_window_knn_on_mnist_data():
     kernel = Kernel.COSINE
     print("+" * 40, "WINDOW KNN on MNIST Data with {} Kernel".format(kernel), "+" * 40)
-    data = get_mnist_images_features(percentage=10)
-    demo_wrapper(data, kernel, mode=KNNMode.RADIUS, k_list=[0.1, 0.5, 0.8, 0.9, 1], n_jobs=10, verbose=1)
+    data = get_mnist_images_features(percentage=20)
+    demo_wrapper(data, kernel, mode=KNNMode.RADIUS, k_list=[0.83], n_jobs=10, verbose=1)
     print("+" * 40, "WINDOW KNN on MNIST Data with {} Kernel".format(kernel), "+" * 40)
     print()
 
